9be2bdf9aa3531e0b210bc9162af059726558725
rsingh_metapack <rahul.singh@metapack.com>
2014-12-11 15:40:23 +0000

    Commit the Spock test for ImmutablePoint

---
+++ src/test/groovy/org/gradle/ImmutablePointTest.groovy
@@ -0,0 +1,44 @@
+package org.gradle
+
+import spock.lang.Specification
+
+/**
+ * Spock test which tests the immutability of {@code ImmutablePoint}.
+ * @author rsingh
+ *
+ */
+class ImmutablePointTest extends Specification {
+    ImmutablePoint p = new ImmutablePoint(x:3, y:4)
+
+    /**
+     * We should be able to create a map using the two property
+     * values of {@code ImmutablePoint}.
+     * 
+     * @return test failure or success
+     */
+    def "can use map creator for immutables"() {
+        expect: [3, 4] == [p.x, p.y]
+    }
+
+    /**
+     * We should not be able to change the value of
+     * property x in the {@code ImmutablePoint}.
+     *
+     * @return test failure or success
+     */
+    def "can't change x"() {
+       when: p.x = 5
+       then: thrown(ReadOnlyPropertyException)
+    }
+
+    /**
+     * We should not be able to change the value of
+     * property y in the {@code ImmutablePoint}.
+     *
+     * @return test failure or success
+     */
+    def "can't change y"() {
+        when: p.y = 9
+        then: thrown(ReadOnlyPropertyException)
+    }
+}
