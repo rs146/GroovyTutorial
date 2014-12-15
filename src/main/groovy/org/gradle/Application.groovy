f10c1ac4c87523cbaf6e6b30ff9a00c9b940c3c1
rsingh_metapack <rahul.singh@metapack.com>
2014-12-15 14:38:49 +0000

    I think I got it working - need to do a unit test to prove it, but we can run the Spring Boot App with Groovy.

--- src/main/groovy/org/gradle/Application.groovy
+++ src/main/groovy/org/gradle/Application.groovy
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @ComponentScan("org.gradle.dataaccess.*")
 @EnableAutoConfiguration
 class Application {
